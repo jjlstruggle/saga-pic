1265291e5f4ec57d93b715681b2ef649a44805b4
秦海峰cq <9802591+qin-haifeng-cq@user.noreply.gitee.com>
2022-06-19 00:15:02 +0800

    点赞

--- app/controller/detailedWorks.js
+++ app/controller/detailedWorks.js
@@ -20,8 +20,8 @@ class detailedWordController extends Controller {
     }
     async goodsNumberAddController() {
         const ctx = this.ctx;
-        const { goods_id } = ctx.request.body
-        await ctx.service.detailedWorks.goodsNumberAdd(goods_id)
+        const { goods_id,myself_id} = ctx.request.body
+        await ctx.service.detailedWorks.goodsNumberAdd(goods_id,myself_id)
         ctx.body = {
             code: 200,
             status: 'success'
