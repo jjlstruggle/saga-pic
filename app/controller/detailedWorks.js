667564f167b87c02d63fd0d7b83fe5bf4ecea474
jjlstruggle <2020213994@stu.cqupt.edu.cn>
2022-06-19 03:10:59 +0800

    update

--- app/controller/detailedWorks.js
+++ app/controller/detailedWorks.js
@@ -4,12 +4,12 @@ class detailedWordController extends Controller {
 
     async getDetailedWorkController() {
         const { ctx } = this
-        const { goods_id,user_id } = ctx.query
-        await ctx.service.detailedWorks.createClick(goods_id,user_id)
+        const { goods_id, user_id } = ctx.query
+        await ctx.service.detailedWorks.createClick(goods_id, user_id)
         const result = await ctx.service.detailedWorks.getDetailedWork(goods_id)
-        const isClick=ctx.service.isClick.getIsClick(goods_id,user_id)
+        const isClick = ctx.service.isClick.getIsClick(goods_id, user_id)
         console.log(isClick);
-        result["isClick"]=isClick.isClick
+        result["isClick"] = isClick.isClick
         console.log(result);
         ctx.status = 200;
         ctx.body = {
@@ -21,17 +21,17 @@ class detailedWordController extends Controller {
     }
     async goodsNumberAddController() {
         const ctx = this.ctx;
-        const { goods_id,myself_id} = ctx.request.body
-       let {goods_num}=await ctx.service.detailedWorks.goodsNumberAdd(goods_id,myself_id)
+        const { goods_id, myself_id } = ctx.request.body
+        let { goods_num } = await ctx.service.detailedWorks.goodsNumberAdd(goods_id, myself_id)
         ctx.body = {
             code: 200,
-            data:goods_num,
+            data: goods_num,
             status: 'success'
         };
     }
     async goodsPriseAddController() {
         const ctx = this.ctx
-        const { goods_id } = ctx.request.body
+        const { goods_id, myself_id } = ctx.request.body
         await ctx.service.detailedWorks.goodsPriseAdd(goods_id)
         ctx.body = {
             code: 200,
