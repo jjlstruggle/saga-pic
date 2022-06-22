a6b08291d9557b0d2885492674c291e0520f3a9f
jjlstruggle <2020213994@stu.cqupt.edu.cn>
2022-06-22 14:34:45 +0800

    update

--- app/controller/home.js
+++ app/controller/home.js
@@ -5,7 +5,7 @@ const Controller = require('egg').Controller;
 class HomeController extends Controller {
   async getUserInfo() {
     const { ctx, service } = this;
-    const user_id = ctx.request.query
+    const { user_id } = ctx.request.query
     const data = await service.home.getUserInfo(user_id)
     ctx.body = {
       code: 200,
